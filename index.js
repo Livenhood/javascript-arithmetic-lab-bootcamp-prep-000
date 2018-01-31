fc294b715d8e2a6422c90734424fa744011ade0f
Michelle Livengood <ml2876@nyu.edu>
2018-01-31 19:32:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function divide(a,b) {
   return a/b
 }
 
-function inc(a, 1) {
-  return a += 1 
+function inc(n) {
+  return n + 1 
 }
 
-function dec(a) {
-  return a -= 1
+function dec(n) {
+  return n - 1
 }
